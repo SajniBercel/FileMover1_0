5a150eaf4d5a56703badfe3b3d9d31ff942dc400
SajniBercel <bercel@srac.hu>
2022-07-05 19:24:00 +0200

    Initial commit

---
+++ FileMover_2_0/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace FileMover_2_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 154);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.listBox3);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 240);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 209);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(301, 81);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(48, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "törlés";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(115, 81);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(48, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "törlés";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(192, 152);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 23);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(192, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(103, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Hozzáad";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Hozzáad";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(103, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mappák";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kiterjesztések";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.listBox4);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(398, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 169);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(260, 88);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "törlés";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Véglegesítés";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(133, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(6, 50);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(248, 109);
+            this.listBox4.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(192, 37);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(103, 109);
+            this.listBox3.TabIndex = 2;
+            this.listBox3.DoubleClick += new System.EventHandler(this.listBox3_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(6, 37);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(103, 109);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Fájlokat tartalmazó mappa kiválasztása";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Célmappákat tartalmazó mappa kiválasztása";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(249, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 455);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(776, 24);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "CsodaCsinálás";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(573, 189);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(148, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Törlés az eredeti helyről";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 173);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 491);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox2;
+        private ListBox listBox4;
+        private ComboBox comboBox1;
+        private ListBox listBox3;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button button3;
+        private Button button4;
+        private CheckBox checkBox1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private Button button6;
+        private Button button5;
+        private TextBox textBox3;
+        private Button button8;
+        private Button button7;
+        private Button button9;
+    }
+}
---
+++ FileMover_2_0/Form1.cs
@@ -0,0 +1,258 @@
+namespace FileMover_2_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FileMover";
+        static string pathPaterns = filepath + @"\Paterns.txt";
+        static string pathOptions;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BeolvasPaterns();
+        }
+        string ForrasMappa;
+        string CelMappa;
+        static List<string> Mappak = new List<string>();
+        static List<string> Fileok = new List<string>();
+        static List<string> kiterjesztesek = new List<string>();
+        static List<patern> Paterns = new List<patern>();
+        private void BeolvasPaterns()
+        {
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+            if (!File.Exists(pathPaterns))
+            {
+                StreamWriter sw = new StreamWriter(pathPaterns);
+                sw.Close();
+            }
+
+            StreamReader f = new StreamReader(pathPaterns);
+            string[] sor;
+            while (!f.EndOfStream)
+            {
+                sor = f.ReadLine().Split(";");
+                patern p = new patern(sor[0], sor[1]);
+                Paterns.Add(p);
+            }
+            f.Close();
+            listBox4.Items.Clear();
+
+            for (int i = 0; i < Paterns.Count; i++)
+            {
+                listBox4.Items.Add(Paterns[i].kiterj + "\t-\t" + Paterns[i].mapp);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fileok.Clear();
+            folderBrowserDialog1.ShowDialog();
+            ForrasMappa = folderBrowserDialog1.SelectedPath + @"\";
+            string[] temp = Directory.GetFiles(ForrasMappa);
+            for (int i = 0; i < temp.Length; i++) { Fileok.Add(temp[i]); }
+            textBox2.Text = ForrasMappa;
+            ListBoxFeltoltoFile(listBox1, Fileok);
+            KiterjesztesRendezo();
+            ComboBoxFeltoltes(comboBox1, kiterjesztesek);
+        }
+        private void ListBoxFeltoltoFile(ListBox listbox, List<string> list)
+        {
+            listbox.Items.Clear();
+            for (int i = 0; i<list.Count; i++)
+            {
+                listbox.Items.Add(Path.GetFileName(list[i]));
+            }
+        }
+        private void ListBoxFeltolto(ListBox listbox, List<string> list)
+        {
+            listbox.Items.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                listbox.Items.Add(list[i]);
+            }
+        }
+        private void KiterjesztesRendezo()
+        {
+            List<string> temp = new List<string>();
+            List<string> list = new List<string>();
+            for (int i = 0; i < Fileok.Count; i++) { list.Add(Fileok[i].Split(".")[Fileok[i].Split(".").Length-1]); }
+
+            //kiválogatás
+            temp.Add(list[0]);
+            bool a = true;
+            for (int i = 0; i<list.Count; i++)
+            {
+                a = true;
+                for (int z = 0; z<temp.Count; z++)
+                {
+                    if (list[i] == temp[z])
+                    {
+                        a = false;
+                    }
+                }
+                if (a)
+                {
+                    temp.Add(list[i]);
+                }
+            }
+
+            //végleges kivitel
+            kiterjesztesek.Clear();
+            for (int i = 0; i<temp.Count; i++)
+            {
+                kiterjesztesek.Add(temp[i]);
+            }
+            ListBoxFeltolto(listBox2, kiterjesztesek);
+        }
+        private void ComboBoxFeltoltes(ComboBox combobox, List<string> list)
+        {
+            combobox.Items.Clear();
+            comboBox1.Text = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                combobox.Items.Add(list[i]);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mappak.Clear();
+            folderBrowserDialog1.ShowDialog();
+            CelMappa = folderBrowserDialog1.SelectedPath + @"\";
+            textBox1.Text = CelMappa;
+            string[] temp = Directory.GetDirectories(CelMappa);
+            for (int i = 0; i < temp.Length; i++) { Mappak.Add(temp[i]); }
+            ListBoxFeltoltoFile(listBox3, Mappak);
+            List<string> temp1 = new List<string>();
+            for (int i = 0; i < Mappak.Count; i++) { temp1.Add(Path.GetFileName(Mappak[i])); }
+            ComboBoxFeltoltes(comboBox2, temp1);
+        }
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            comboBox1.Text = listBox2.SelectedItem.ToString();
+        }
+
+        private void listBox3_DoubleClick(object sender, EventArgs e)
+        {
+            comboBox2.Text = listBox3.SelectedItem.ToString();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && comboBox2.Text != "")
+            {
+                patern j = new patern(comboBox1.Text, comboBox2.Text);
+                bool a = true;
+                for (int i = 0; i<Paterns.Count; i++)
+                {
+                    if (Paterns[i].kiterj == j.kiterj && Paterns[i].mapp == j.mapp)
+                    {
+                        a = false;
+                    }
+                }
+                if (a)
+                {
+                    Paterns.Add(j);
+                }
+            }
+            
+            listBox4.Items.Clear();
+
+            for(int i = 0; i<Paterns.Count; i++)
+            {
+                listBox4.Items.Add(Paterns[i].kiterj + "\t-\t" + Paterns[i].mapp);
+            }
+            PaternSave();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i<Paterns.Count; i++)
+            {
+                for (int z = 0; z<Fileok.Count; z++)
+                {
+                    string kiterjeszt = Fileok[z].Split(".")[Fileok[z].Split(".").Length - 1];
+                    if (!Directory.Exists(Path.Combine(CelMappa, Paterns[i].mapp)))
+                    {
+                        Directory.CreateDirectory(Path.Combine(CelMappa, Paterns[i].mapp));
+                    }
+                    if (Paterns[i].kiterj == kiterjeszt)
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            File.Move(Path.Combine(ForrasMappa, Path.GetFileName(Fileok[z])), Path.Combine(CelMappa, Paterns[i].mapp) + @"\" + Path.GetFileName(Fileok[z]), true);
+                        }
+                        else
+                        {
+                            File.Copy(Path.Combine(ForrasMappa, Path.GetFileName(Fileok[z])), Path.Combine(CelMappa, Paterns[i].mapp) + @"\" + Path.GetFileName(Fileok[z]), true);
+                        }
+                    }
+                }
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            kiterjesztesek.Add(textBox3.Text);
+            ComboBoxFeltoltes(comboBox1, kiterjesztesek);
+            ListBoxFeltolto(listBox2, kiterjesztesek);
+            textBox3.Text = "";
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Mappak.Add(CelMappa+@"\"+textBox4.Text);
+            List<string> temp = new List<string>();
+            for (int i = 0; i < Mappak.Count; i++) { temp.Add(Path.GetFileName(Mappak[i])); }
+            ComboBoxFeltoltes(comboBox2, temp);
+            ListBoxFeltoltoFile(listBox3, Mappak);
+            textBox4.Text = "";
+        }
+        private void DeletFromListbox(ListBox listbox)
+        {
+            if (listbox.SelectedIndex != -1)
+            {
+                listbox.Items.RemoveAt(listbox.SelectedIndex);
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+                kiterjesztesek.RemoveAt(listBox2.SelectedIndex);
+            DeletFromListbox(listBox2);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (listBox3.SelectedIndex != -1)
+                Mappak.RemoveAt(listBox3.SelectedIndex);
+            DeletFromListbox(listBox3);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (listBox4.SelectedIndex != -1)
+                Paterns.RemoveAt(listBox4.SelectedIndex);
+            DeletFromListbox(listBox4);
+            PaternSave();
+        }
+        private void PaternSave()
+        {
+            StreamWriter z = new StreamWriter(pathPaterns);
+            for (int i = 0; i<Paterns.Count;i++)
+            {
+                z.WriteLine(Paterns[i].kiterj+";"+Paterns[i].mapp);
+            }
+            z.Close();
+        }
+    }
+    public class patern
+    {
+        public string kiterj { get; set; }
+        public string mapp { get; set; }
+        public patern(string a, string b)
+        { 
+            kiterj = a;
+            mapp = b;
+        }
+    }
+}
